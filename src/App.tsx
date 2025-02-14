9c6fdc167b07966c4cad1f7845c3560357e9e22e
diego1999dd <diego.9090dd@gmail.com>
2025-02-14 11:10:43 -0300

    :tada: Commit inicial, estilizei a Home, e estou fazendo a Navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import Home from './pages/home/home';
+import Navbar from './pages/components/navbar/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
     </>
   );
---
+++ src/pages/components/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+function Navbar() {
+  return (
+    <div
+      className="
+    flex
+    w-full
+    h-12
+    bg-indigo-900
+    "
+    >
+      <div
+        className="
+       flex
+       pl-5
+       pt-3
+       text-white
+       font-normal
+      "
+      >
+        Blog Pessoal
+      </div>
+      <div
+        className="
+        flex
+        
+        
+        "
+      ></div>
+    </div>
+  );
+}
+
+export default Navbar;
